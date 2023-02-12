013f6c9e800681bdfef5c63d9b272f7f0f9033fe
thanhtutzaw <zgyi5025@gmail.com>
2023-02-12 17:23:52 +0630

    added pin button fun

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import './App.css';
 import Task from './components/Task.jsx';
 
 function App() {
-  // const task = {
-
-  // }
-
   const [task, settask] = useState(
     [{
       id: 1,
       title: 'hello world',
-      // state: 'TASK_ARCHIVED'
       state: 'TASK_INBOX'
     },
     {
       id: 2,
       title: 'hedfdfsdfdsd',
-      // state: 'TASK_ARCHIVED'
       state: 'TASK_INBOX'
     }
       , {
       id: 3,
       title: 'hello ',
-      // state: 'TASK_ARCHIVED'
       state: 'TASK_INBOX'
     }]
   );
@@ -37,18 +30,21 @@ function App() {
     )
   }
   function onPinTask(id) {
-    alert(id)
+    settask(
+      task.map(t => t.id === id ? t.state !== 'TASK_PINNED' ? { ...t, state: 'TASK_PINNED' } 
+      : 
+      { ...t, state: 'TASK_INBOX' } : 
+      t
+      )
+    )
   }
   return (
     <div className="App">
       {task.map((t) => (
         <Task key={t.id} onPinTask={onPinTask} onArchiveTask={onArchiveTask} task={t} />
       ))}
-      {/* <Task onPinTask={onPinTask} onArchiveTask={onArchiveTask} task={task} /> */}
-      {/* <Task onPinTask={onPinTask} onArchiveTask={onArchiveTask} task={task} /> */}
     </div>
   );
 }
 
 export default App;
-
